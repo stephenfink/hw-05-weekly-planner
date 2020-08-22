53ee9ab55e2319973c9d6582c9a57e2d124644aa
Stephen Fink <stephenfink8@gmail.com>
2020-08-22 17:29:34 -0500

    got final set up for events and and had issues with local storage due to current time

--- Develop/weekly.js
+++ Develop/weekly.js
@@ -19,32 +19,30 @@ $("#currentDay").text(arrayDate[0] + ", " + arrayDate[1] + " " + arrayDate[2]);
     // this is for events saving for local storage 
 
     function renderEvents(){
-        events = JSON.parse(localStorage.getItem("#eventsInput"));
+        events = JSON.parse(localStorage.getItem("#eventInput"));
     
         if( events === null){
             events = [
-            {//i-index is 0 for 6 am
-                event: ""},
             {//i-index is 1 for 7 am
-                event: ""},
+                ventInput: ""},
             {//i-index is 2 for 8 am
-                event: ""},
+                eventInput: ""},
             {//i-index is 3 for 9 am
-                event: ""},
+                eventInput: ""},
             {//i-index is 4 for 10 am
-                event: ""},
+                eventInput: ""},
             {//i-index is 5 for 11 am
-                event: ""},
+                eventInput: ""},
             {//i-index is 6 for 12 pm
-                event: ""},
+                eventInput: ""},
             {//i-index is 7 for 1 pm
-                event: ""},
+                eventInput: ""},
             {//i-index is 8 for 2 pm
-                event: ""},
+                eventInput: ""},
             {//i-index is 9 for 3 pm
-                event: ""},
+                eventInput: ""},
             {//i-index is 10  for 4 pm
-                event: ""}
+                eventInput: ""}
             ];
             localStorage.setItem("#eventInput", JSON.stringify(events));
         } else{ 
@@ -57,12 +55,12 @@ $("#currentDay").text(arrayDate[0] + ", " + arrayDate[1] + " " + arrayDate[2]);
     renderEvents();
 //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // this changes the colors base on time, 
-    function colors(){
+    function colorsRender(){
         for(var i = 0; i<events.length; i++){
             var sel = $("#eventInput" + (i+1));
 
             if(sel[0].previousElementSibling.attributes[1].value < currentTime[0]){
-
+                 // the css has some colors madde already to do this but when and how it is use is what code here needs
                 sel[0].classList.add("past");
                 sel[0].previousElementSibling.classList.add("past");
                 sel[0].classList.add("past");
@@ -84,41 +82,34 @@ $("#currentDay").text(arrayDate[0] + ", " + arrayDate[1] + " " + arrayDate[2]);
                 sel[0].previousElementSibling.classList.add("future");
     
             }
+            //WHEN I scroll down: how many hours to use within one day: checked by HTML
+
+//THEN I am presented with timeblocks for standard business hours
+    // for each hour: 12 hour work flow to set for over time or change of time worked
     }
 }
-colors();
+colorsRender();
+
+// this is for the save button functions for each time slot as its own
+//however so far it does not save if it is past or cuttent
 $(".btn").click(function(){
     var btnNum = this.id.match(/\d+/g).map(Number);
     
     events[btnNum - 1].event = this.previousElementSibling.value;
     
-    localStorage.setItem("events", JSON.stringify(events));
-});
 
 
-
-    // the css has some colors madde already to do this but when and how it is use is what code here needs
-
-
-//WHEN I scroll down: how many hours to use within one day: checked by HTML
-
-//THEN I am presented with timeblocks for standard business hours
-    // for each hour: 12 hour work flow to set for over time or change of time worked
-
-
-
-
-
-
-//WHEN I click into a timeblock:
-    // on click event for a input event to reflect user input to be shown on screen
+    
+    localStorage.setItem("eventInput", JSON.stringify(events));
+});//WHEN I click into a timeblock:
+// on click event for a input event to reflect user input to be shown on screen
 
 //THEN I can enter an event:
-    // event is saved for use later, as well as a past,current, and future events of that day
+// event is saved for use later, as well as a past,current, and future events of that day
 
 //WHEN I click the save button for that timeblock
-    //This will be an event listener for clicking save to save data to storage
-    //the storage will be locked into that time frame
+//This will be an event listener for clicking save to save data to storage
+//the storage will be locked into that time frame
 //THEN the text for that event is saved in local storage
 
 
@@ -127,3 +118,17 @@ $(".btn").click(function(){
 
 //WHEN I refresh the page
 //THEN the saved events persist 
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
